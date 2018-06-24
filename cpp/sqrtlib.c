4f00177f12c774296f43106754b63ef798389ea4
LeoFuso <leonardofusonuzzo@gmail.com>
2018-06-24 13:39:54 -0300

    Removido descrição desnecessária

--- cpp/sqrtlib.c
+++ cpp/sqrtlib.c
@@ -9,119 +9,119 @@ static PyObject *
 calculate_wrapper(PyObject *self, PyObject *args)
 {
 
-    /* The following element of an array */
-    PyObject *sequence;
+	/* The following element of an array */
+	PyObject *sequence;
 
-    /* the Python Array supplier */
-    double *array;
+	/* the Python Array supplier */
+	double *array;
 
-    /* the result of the function */
-    double result;
+	/* the result of the function */
+	double result;
 
-    /* the sequence length */
-    unsigned int sequence_length;
+	/* the sequence length */
+	unsigned int sequence_length;
 
-    /*
-     * get one argument as a sequence
-     * the '0' stands for the data type expected
-     */
-    if (!PyArg_ParseTuple(args, "0", &sequence))
-        return NULL;
+	/*
+	 * get one argument as a sequence
+	 * the '0' stands for the data type expected
+	 */
+	if (!PyArg_ParseTuple(args, "0", &sequence))
+		return NULL;
 
-    sequence = PySequence_Fast(sequence, "argument must be iterable");
-    if (!sequence)
-        return NULL;
+	sequence = PySequence_Fast(sequence, "argument must be iterable");
+	if (!sequence)
+		return NULL;
 
-    /*
-     * prepare data as an array of doubles
-     * Compiler warns that Py_ssize_t differs from unsigned int
-     */
-    sequence_length = PySequence_Fast_GET_SIZE(sequence);
+	/*
+	 * prepare data as an array of doubles
+	 * Compiler warns that Py_ssize_t differs from unsigned int
+	 */
+	sequence_length = PySequence_Fast_GET_SIZE(sequence);
 
-    array = (double *) calloc(sequence_length, sizeof(double));
+	array = (double *) calloc(sequence_length, sizeof(double));
 
-    if (!array)
-    {
-        Py_DECREF(sequence);
-        return PyErr_NoMemory();
-    }
+	if (!array)
+	{
+		Py_DECREF(sequence);
+		return PyErr_NoMemory();
+	}
 
-    /*
-     * Starts filling up the array
-     * This can be optimized
-     */
-    unsigned int i;
-    for (i = 0; i < sequence_length; i++)
-    {
+	/*
+	 * Starts filling up the array
+	 * This can be optimized
+	 */
+	unsigned int i;
+	for (i = 0; i < sequence_length; i++)
+	{
 
-        /* float value */
-        PyObject *f_item;
+		/* float value */
+		PyObject *f_item;
 
-        /* item of the python array */
-        PyObject *item = PySequence_Fast_GET_ITEM(sequence, i);
-        if (!item)
-        {
-            Py_DECREF(sequence);
-            free(array);
-            return NULL;
-        }
+		/* item of the python array */
+		PyObject *item = PySequence_Fast_GET_ITEM(sequence, i);
+		if (!item)
+		{
+			Py_DECREF(sequence);
+			free(array);
+			return NULL;
+		}
 
-        f_item = PyNumber_Float(item);
+		f_item = PyNumber_Float(item);
 
-        if (!f_item)
-        {
-            Py_DECREF(sequence);
-            free(array);
-            PyErr_SetString(PyExc_TypeError, "all items must be numbers");
-            return NULL;
-        }
-        array[i] = PyFloat_AsDouble(f_item);
-        Py_DECREF(f_item);
-    }
+		if (!f_item)
+		{
+			Py_DECREF(sequence);
+			free(array);
+			PyErr_SetString(PyExc_TypeError, "all items must be numbers");
+			return NULL;
+		}
+		array[i] = PyFloat_AsDouble(f_item);
+		Py_DECREF(f_item);
+	}
 
-    /* clean up */
-    Py_DECREF(sequence);
+	/* clean up */
+	Py_DECREF(sequence);
 
-    /* compute */;
-    result = calculate(array);
+	/* compute */;
+	result = calculate(array);
 
-    free(array);
+	free(array);
 
-    /*
-     * and return the result
-     * the 'd' stands for Double data type
-     */
-    return Py_BuildValue("d", result);
+	/*
+	 * and return the result
+	 * the 'd' stands for Double data type
+	 */
+	return Py_BuildValue("d", result);
 }
 
 static PyObject *
-calculate_single_wrapper(PyObject *self, PyObject *args){ /* empty */ }
+calculate_single_wrapper(PyObject *self, PyObject *args) { /* empty */ }
 
 static PyMethodDef module_methods_list[] =
-    {
-        {"calculate", (PyCFunction) calculate_wrapper, METH_VARARGS, "Sum of a sequence of square roots of numbers."},
-        {"calculateSingle", (PyCFunction)calculate_single_wrapper, METH_VARARGS, "Return the square root of a number."},
-        {NULL, NULL, 0, NULL} /* sentinel */
-    };
+	{
+		{"calculate", (PyCFunction) calculate_wrapper, METH_VARARGS, NULL},
+		{"singleCalculate", (PyCFunction) calculate_single_wrapper, METH_VARARGS, NULL},
+		{NULL, NULL, 0, NULL} /* sentinel */
+	};
 
 /*
  * @see https://docs.python.org/3/c-api/module.html#c.PyModuleDef
  */
 static struct PyModuleDef moduleDef =
-    {
-        PyModuleDef_HEAD_INIT,
-        "SquareRoot",           /* name of module */
-        "Some description",     /* module documentation, may be NULL */
-        -1,                     /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
-        module_methods_list,
-        NULL,
-        NULL,
-        NULL,
-        NULL
-    };
+	{
+		PyModuleDef_HEAD_INIT,
+		"SquareRoot",           /* name of module */
+		"Some description",     /* module documentation, may be NULL */
+		-1,                     /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+		module_methods_list,
+		NULL,
+		NULL,
+		NULL,
+		NULL
+	};
 
 PyMODINIT_FUNC
 PyInit_SquareRoot()
 {
-    return PyModule_Create(&moduleDef);
+	return PyModule_Create(&moduleDef);
 }
