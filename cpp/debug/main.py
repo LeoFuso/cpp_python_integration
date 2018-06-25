bf0dc3c80b8a4989295578a5a90df71c7c5c50b7
LeoFuso <leonardofusonuzzo@gmail.com>
2018-06-25 08:48:58 -0300

    Agora o main em python é mais complexo e realiza duas operações, uma da qual funciona perfeitamente.

--- cpp/debug/main.py
+++ cpp/debug/main.py
@@ -1,4 +1,12 @@
-from SquarePy import calculate
+import SquarePy
+
 arr = [1, 2, 3, 4, 5, 6]
-result = calculate(arr)
+value = 25.0
+
+print("Calculate Single of:\nVALUE: ",value)
+result = SquarePy.calculate_single(value)
+print("RESULT: ",result)
+print("Calculate Sequence of:\nVALUE: ",arr)
+result = SquarePy.calculate_sequence(arr)
+print("RESULT: ",result)
 print(result)
